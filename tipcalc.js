5d28705d12bc2806726899ff2e55b13d4db6329e
Saliya Seneviratne <sseneviratne@thinkful.com>
2015-06-08 18:02:42 -0400

    clean up formatting

--- tipcalc.js
+++ tipcalc.js
@@ -1,57 +1,51 @@
 angular.module('tipApp', ['ngRoute'])
-    .config(function($routeProvider) {
-        $routeProvider
-        .when('/', {
-          templateUrl : 'home.html',
-        })
-        .when('/new-meal', {
-          templateUrl : 'new-meal.html',
-          controller : 'NewMealsCtrl'
-        })
-        .when('/my-earnings', {
-          templateUrl : 'my-earnings.html',
-          controller : 'EarningsCtrl'
-        })
-        .when('/error', {
-          template : '<p>Error - Page Not Found</p>'
-        })
-        .otherwise({redirectTo : '/route'});
-
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'home.html',
       })
-      .controller('MainCtrl', function($route, $routeParams) {
-           
+      .when('/new-meal', {
+        templateUrl: 'new-meal.html',
+        controller: 'NewMealsCtrl'
       })
-      .controller('HomeCtrl', function($routeParams) {
-        
-      
+      .when('/my-earnings', {
+        templateUrl: 'my-earnings.html',
+        controller: 'EarningsCtrl'
       })
-
-      .controller('NewMealsCtrl', function($routeParams) {
-             $scope.submitForm = function() {
-
-               $scope.tiptotal = $scope.basemeal * $scope.tippercent/100;
-
-               $scope.taxtotal = $scope.basemeal * $scope.taxrate/100 + $scope.basemeal;
-
-               $scope.mealtotal = $scope.taxtotal + $scope.tiptotal;
-
-               $scope.fulltiptotal = $scope.fulltiptotal + $scope.tiptotal;
-
-               $scope.mealcount = $scope.mealcount + 1;
-
-               $scope.avgtip = $scope.fulltiptotal/$scope.mealcount;
-
-         };
-
+      .when('/error', {
+        template: '<p>Error - Page Not Found</p>'
       })
-      .controller('EarningsCtrl', function($scope) {
-        
-         $scope.clearfields = function() {
-                  $scope.basemeal = "";
-                  $scope.taxrate = "";
-                  $scope.tippercent = "";
-         };
+      .otherwise({redirectTo: '/'});
+  })
 
-      }); 
+  .controller('MainCtrl', function($scope, $routeParams) {
 
+  })
 
+  .controller('NewMealsCtrl', function($scope, $routeParams) {
+    $scope.submitForm = function() {
+
+      $scope.tiptotal = $scope.basemeal * $scope.tippercent/100;
+
+      $scope.taxtotal = $scope.basemeal * $scope.taxrate/100 + $scope.basemeal;
+
+      $scope.mealtotal = $scope.taxtotal + $scope.tiptotal;
+
+      $scope.fulltiptotal = $scope.fulltiptotal + $scope.tiptotal;
+
+      $scope.mealcount = $scope.mealcount + 1;
+
+      $scope.avgtip = $scope.fulltiptotal/$scope.mealcount;
+
+    };
+
+  })
+  .controller('EarningsCtrl', function($scope) {
+
+    $scope.clearfields = function() {
+      $scope.basemeal = "";
+      $scope.taxrate = "";
+      $scope.tippercent = "";
+    };
+
+  });
